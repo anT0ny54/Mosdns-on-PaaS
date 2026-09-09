e829ec1b8166b63b969663278366e7677a23e147
anT0ny54 <109664822+anT0ny54@users.noreply.github.com>
2026-09-09 16:56:56 +0700

    Add files via upload

--- content/entrypoint.sh
+++ content/entrypoint.sh
@@ -1,7 +1,19 @@
 #!/bin/sh
+set -eu
 
-sh /etc/mosdns/install_geodata.sh
+CONFIG=/etc/mosdns/config.yaml
+PORT="${PORT:-8080}"
+DOH_PATH="${DOH_PATH:-/dns-query}"
 
-sed -i "s|PORT_PLACEHOLDER|${PORT}|;s|PATH_PLACEHOLDER|${DOH_PATH}|" /etc/mosdns/config.yaml
+case "$DOH_PATH" in
+  /*) ;;
+  *) DOH_PATH="/$DOH_PATH" ;;
+esac
+
+# Escape values before inserting them into YAML.
+ESCAPED_PORT=$(printf '%s' "$PORT" | sed 's/[\\&|]/\\&/g')
+ESCAPED_PATH=$(printf '%s' "$DOH_PATH" | sed 's/[\\&|]/\\&/g')
+
+sed -i "s|0.0.0.0:8080|0.0.0.0:${ESCAPED_PORT}|; s|path: /dns-query|path: ${ESCAPED_PATH}|" "$CONFIG"
 
 exec mosdns start -d /etc/mosdns
